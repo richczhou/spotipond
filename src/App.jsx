ce7e4bf987a0e541d3b7aa8b554fda76e1617159
Richard Zhou <richczhou@gmail.com>
2023-02-03 18:42:20 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { Canvas } from "@react-three/fiber"
+// import reactLogo from './assets/react.svg'
+
+import './App.css'
+import PondLayout from './PondLayout'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Canvas className="main-canvas" camera={{fov: 50, position:[-0.5, 1.5, 1.5]}} >
+        <PondLayout />
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
