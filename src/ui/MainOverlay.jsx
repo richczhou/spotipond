39b973d05242f3ef3ac8eed56b520d42c36ef582
Richard Zhou <richczhou@gmail.com>
2023-02-04 13:59:23 -0500

    iframe tweaks

--- src/ui/MainOverlay.jsx
+++ src/ui/MainOverlay.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function MainOverlay() {
-    const [isActive, setActive] = useState(0);
+    const [isActive, setActive] = useState(false);
+    const playlistRef = useRef();
+    let isReady = true;
+
+    function pullUp() {
+        setActive(true);
+        // console.log(playlistRef.current)
+        playlistRef.current.style.top = '75%';
+    }
+
+    function pullDown() {
+        setActive(false);
+        playlistRef.current.style.top = '15%';
+    }
 
     return (
     <div className="overlay">
         <div id="main-overlay">
-            <iframe className='playlist'
-                    src="https://open.spotify.com/embed/playlist/5OHAlIRtavyClwEDV8cEp9?utm_source=generator"
-                    frameBorder="0" 
-                    allowFullScreen="" 
-                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                    loading="lazy"
-                    ></iframe>
+            <div className="playlist" 
+                 ref={playlistRef} >
+                <iframe src="https://open.spotify.com/embed/playlist/5OHAlIRtavyClwEDV8cEp9?utm_source=generator"
+                        frameBorder={0} 
+                        allowFullScreen={0} 
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                        loading="lazy" 
+                        onPointerOver={pullUp}
+                        onPointerOut={pullDown} />
+            </div>
         </div>
     </div>
     )
