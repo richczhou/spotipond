0a258b9775613f3a71d5f7d0ae40469514019fad
Richard Zhou <richczhou@gmail.com>
2023-02-04 14:56:23 -0500

    added `Config`

---
+++ src/Config.jsx
@@ -0,0 +1,16 @@
+const spotify = {
+    danceability: 0.5,
+    energy: 0.5,
+    speechiness: 0.5,
+    tempo: 90.0,
+    valence: 0.5,
+    fresh: true, // false if you want all songs, true if you want only 10
+    privUserToken: 'BQBiN_QGKp5KL9a0RTobeT8nMgaZr9ZeM_C0Lv3XF0DqNKiZrDrdRoJKbBrf3zsFCnX0KIjLRn8JXICQf6zEQvDdjph4yYRJtbPqbHWAd22rOEvvoPm_ZKelTU5uHZVCpWHu5iXg8cYk3T0Jlek--IGMhpe-HwnJkVhzKy8jD1kucLzEyobOouagN263',
+    privSongToken: 'BQDhNwNaSp_Usd3xZWFBH0XePagt_1RKEtFHqS11PUpfSCxipC9y5B9c0nteYJm1rKiwzd6aUBDHXZQYy49HT_66wKsDnNg7ed01Bbb02J1LTq_CcpWGeJA9OGzPcH7OiNfpAP39YHcxCiE8dqhvS1vi8H-EmyGBITQjSMaAcxF0',
+    privPlaylistToken: 'BQBvGHyEO6ltYa4p2B0gjW5xMnwdL5KYbAcJgBCPPKPG344p0PcdmdludF4FwmmuzjJiJvqdSa-Um1ZQyigxxZr-8Mg03M3ZvJWQTFndiVHY_5q61nToyDWjEtyAu4xucdfwcElA5w6wycHeUTnKoMYtVAAtOU4Iz8bXHPlaogJoV5NHaWehvi3l9Pd36Vz64CUetCs8Jz2RaiU-vs34RPs6qktTHsDbcaWtvsvQ8A',
+    clientId: 'a56551db3f5d4df7b05e7efa31394e98',
+    redirectUrl: 'http://localhost:5173/callback',
+
+}
+
+export { spotify };
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,25 +1,15 @@
-import { useState, useEffect, useRef } from 'react'
-import './App.css'
-import './Login.css'
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
+import './Login.css';
+import { spotify } from './Config';
 
-window.danceability = 0.5;
-window.energy = 0.5;
-window.speechiness = 0.5;
-window.tempo = 90.0;
-window.valence = 0.5;
-window.fresh = true; // false if you want all songs, true if you want only 10
-window.privUserToken = 'BQBiN_QGKp5KL9a0RTobeT8nMgaZr9ZeM_C0Lv3XF0DqNKiZrDrdRoJKbBrf3zsFCnX0KIjLRn8JXICQf6zEQvDdjph4yYRJtbPqbHWAd22rOEvvoPm_ZKelTU5uHZVCpWHu5iXg8cYk3T0Jlek--IGMhpe-HwnJkVhzKy8jD1kucLzEyobOouagN263';
-window.privSongToken = 'BQDhNwNaSp_Usd3xZWFBH0XePagt_1RKEtFHqS11PUpfSCxipC9y5B9c0nteYJm1rKiwzd6aUBDHXZQYy49HT_66wKsDnNg7ed01Bbb02J1LTq_CcpWGeJA9OGzPcH7OiNfpAP39YHcxCiE8dqhvS1vi8H-EmyGBITQjSMaAcxF0';
-window.privPlaylistToken = 'BQBvGHyEO6ltYa4p2B0gjW5xMnwdL5KYbAcJgBCPPKPG344p0PcdmdludF4FwmmuzjJiJvqdSa-Um1ZQyigxxZr-8Mg03M3ZvJWQTFndiVHY_5q61nToyDWjEtyAu4xucdfwcElA5w6wycHeUTnKoMYtVAAtOU4Iz8bXHPlaogJoV5NHaWehvi3l9Pd36Vz64CUetCs8Jz2RaiU-vs34RPs6qktTHsDbcaWtvsvQ8A';
 
 function Login() {
-    const CLIENT_ID = "a56551db3f5d4df7b05e7efa31394e98"
-    const REDIRECT_URI = "http://localhost:5173/callback"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
 
-    const [token, setToken] = useState("")
-    const [songs, setSongs] = useState([])
+    const [token, setToken] = useState("");
+    const [songs, setSongs] = useState([]);
     const playlistId = useRef("");
 
     useEffect(() => {
@@ -44,17 +34,17 @@ function Login() {
     async function getUser() {
         const result = await fetch(`https://api.spotify.com/v1/me`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + window.privUserToken},
+            headers: { 'Authorization' : 'Bearer ' + spotify.privUserToken},
         });
         const user = await result.json();
         return user.id;
     }
 
     async function getSongs(n) {
-        var recomURL = `${'https://api.spotify.com/v1/recommendations?limit='}${n}${'&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=pop&seed_tracks=0c6xIDDpzE81m2q797ordA'}${'&target_danceability='}${window.danceability}${'&target_energy='}${window.energy}${'&target_speechiness='}${window.speechiness}${'&target_tempo='}${window.tempo}${'&target_valence='}${window.valence}`
+        var recomURL = `${'https://api.spotify.com/v1/recommendations?limit='}${n}${'&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=pop&seed_tracks=0c6xIDDpzE81m2q797ordA'}${'&target_danceability='}${spotify.danceability}${'&target_energy='}${spotify.energy}${'&target_speechiness='}${spotify.speechiness}${'&target_tempo='}${spotify.tempo}${'&target_valence='}${spotify.valence}`
         const result = await fetch(recomURL, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + window.privSongToken},
+            headers: { 'Authorization' : 'Bearer ' + spotify.privSongToken},
         })
         const data = await result.json()
 
@@ -95,40 +85,41 @@ function Login() {
     }
 
     async function createPlaylist() {
-        if (token) {
-            getUser().then(
-                async function (id) {
-                    var playlistUrl = `${'https://api.spotify.com/v1/users/'}${id}${'/playlists'}`
+        // if (token) {
+        //     getUser().then(
+        //         async function (id) {
+        //             var playlistUrl = `${'https://api.spotify.com/v1/users/'}${id}${'/playlists'}`
 
-                    const result = await fetch(playlistUrl, {
-                    method: 'POST',
-                    headers: { 'Authorization' : 'Bearer ' + window.privPlaylistToken},
-                    body: JSON.stringify({
-                        name: "spotipond finds v8",
-                        description: "here are your fresh finds",
-                        public: false,
-                      }),                 
-                    })
+        //             const result = await fetch(playlistUrl, {
+        //             method: 'POST',
+        //             headers: { 'Authorization' : 'Bearer ' + spotify.privPlaylistToken},
+        //             body: JSON.stringify({
+        //                 name: "spotipond finds v8",
+        //                 description: "here are your fresh finds",
+        //                 public: false,
+        //               }),                 
+        //             })
 
-                    const data = await result.json();
-                    // console.log(data);
-                    playlistId.current = data.id;
-                    const songUri = parseSongs();
+        //             const data = await result.json();
+        //             // console.log(data);
+        //             playlistId.current = data.id;
+        //             const songUri = parseSongs();
 
-                    var addSongsUrl = `${'https://api.spotify.com/v1/playlists/'}${playlistId.current}${'/'}`
+        //             var addSongsUrl = `${'https://api.spotify.com/v1/playlists/'}${playlistId.current}${'/'}`
 
-                    const result2 = await fetch(addSongsUrl + songUri, {
-                    method: 'POST',
-                    headers: { 'Authorization' : 'Bearer ' + window.privPlaylistToken},                
-                    })
+        //             const result2 = await fetch(addSongsUrl + songUri, {
+        //             method: 'POST',
+        //             headers: { 'Authorization' : 'Bearer ' + spotify.privPlaylistToken},                
+        //             })
 
-                    const data2 = await result2.json();
-                },
-                (e) => {
-                  console.error(e); 
-                },
-            );
-        }
+        //             const data2 = await result2.json();
+        //         },
+        //         (e) => {
+        //           console.error(e); 
+        //         },
+        //     );
+        // }
+        playlistId.current = '3TAWFddAhxONhW6o2OVYuW';
     }
 
     if (songs.length >= 10) {
@@ -173,7 +164,7 @@ function Login() {
                     : 
                     <>
                         <h2>Please login to enable experience</h2>
-                        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login</a>
+                        <a href={`${AUTH_ENDPOINT}?client_id=${spotify.clientId}&redirect_uri=${spotify.redirectUrl}&response_type=${RESPONSE_TYPE}`}>Login</a>
                     </>
                 }
         </div>
