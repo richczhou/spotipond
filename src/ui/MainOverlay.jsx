f8b7598e2dde0bcf793c2c97e262296f34c6113e
Richard Zhou <richczhou@gmail.com>
2023-02-04 14:12:53 -0500

    iframe tweaks

--- src/ui/MainOverlay.jsx
+++ src/ui/MainOverlay.jsx
@@ -2,18 +2,49 @@ import { useRef, useState } from "react";
 
 function MainOverlay() {
     const [isActive, setActive] = useState(false);
+    const [isReady, setReady] = useState(true);
     const playlistRef = useRef();
-    let isReady = true;
 
     function pullUp() {
+        if (!isReady) return;
+        setReady(false);
+
         setActive(true);
         // console.log(playlistRef.current)
-        playlistRef.current.style.top = '75%';
+        // playlistRef.current.style.top = '15%';
+        playlistRef.current.animate([
+            // keyframes
+            { transform: 'translateY(0%)' },
+            { transform: 'translateY(-60%)' }
+          ], {
+            // timing options
+            duration: 300,
+            iterations: 1
+        });
+        setTimeout(() => {
+            playlistRef.current.style.top = '15%';
+        }, 300);  
+
+        setTimeout(() => {
+            setReady(true);
+        }, 500);  
     }
 
     function pullDown() {
         setActive(false);
-        playlistRef.current.style.top = '15%';
+
+        playlistRef.current.animate([
+            // keyframes
+            { transform: 'translateY(0%)' },
+            { transform: 'translateY(60%)' }
+          ], {
+            // timing options
+            duration: 300,
+            iterations: 1
+        });
+        setTimeout(() => {
+            playlistRef.current.style.top = '75%';
+        }, 300);  
     }
 
     return (
