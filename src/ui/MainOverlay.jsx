726ddef8313df08d299b9078d7e71f5f2dade36d
Richard Zhou <richczhou@gmail.com>
2023-02-04 13:09:52 -0500

    iframe testing

--- src/ui/MainOverlay.jsx
+++ src/ui/MainOverlay.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 function MainOverlay() {
+    const [isActive, setActive] = useState(0);
+
     return (
     <div className="overlay">
-        <h3>
-            Fresh Finds
-        </h3>
+        <div id="main-overlay">
+            <iframe className='playlist'
+                    src="https://open.spotify.com/embed/playlist/5OHAlIRtavyClwEDV8cEp9?utm_source=generator"
+                    frameBorder="0" 
+                    allowfullscreen="" 
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                    loading="lazy"
+                    ></iframe>
+        </div>
     </div>
     )
 }
